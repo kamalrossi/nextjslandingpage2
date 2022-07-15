39afaf6ab91499e8676d9abdff653f896f2caf25
Rossi Kamal <rossikamal@gmail.com>
2022-07-16 03:08:46 +0600

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,12 @@
 import { tw, css } from 'twind/css';
 import Button from '@/components/button';
+
 import Netlify from '@/constants/svg/netlify.svg';
-import Nike from '@/constants/svg/nike.svg';
-import Figma from '@/constants/svg/figma.svg';
-import Aws from '@/constants/svg/aws.svg';
+import aliexpress from '@/constants/aliexpress.png';
+import amazon from '@/constants/amazon.png';
+import uniqlo from '@/constants/uniqlo.png';
+import wallmart from '@/constants/wallmart.png';
+
 
 const headerStyle = css`
   background-color: #ffffff;
@@ -14,27 +17,29 @@ const Header = () => (
   <header className={tw(headerStyle)}>
     <div className={tw(`max-w-4xl mx-auto py-16 px-14 sm:px-6 lg:px-8`)}>
       <h1 className={tw(`font-sans font-bold text-4xl md:text-5xl lg:text-8xl text-center leading-snug text-gray-800`)}>
-        Your website, beyond expectations
+       Green Wallet, Everyone's Choice
       </h1>
       <div className={tw(`max-w-xl mx-auto`)}>
         <p className={tw(`mt-10 text-gray-500 text-center text-xl lg:text-3xl`)}>
-          Make your website wonderful and build beyond your expectations.
+          Shopping is one swipe away now!
         </p>
       </div>
       <div className={tw(`mt-10 flex justify-center items-center w-full mx-auto`)}>
-        <Button primary>Get started</Button>
+        <Button primary>Try</Button>
         <span className={tw(`mx-2`)}>or</span>
-        <Button>Contact us</Button>
+        <Button>Register</Button>
       </div>
     </div>
     <div className={tw(`flex justify-center w-full`)}>
       <div className={tw(`mt-4 w-full`)}>
-        <p className={tw(`font-mono uppercase text-center font-medium text-sm text-gray-600`)}>These folks get it</p>
+        <p className={tw(`font-mono uppercase text-center font-medium text-sm text-gray-600`)}>Our Happy Client</p>
         <div className={tw(`flex items-center justify-center mx-auto flex-wrap`)}>
-          <Aws className={tw(`m-12 mb-8`)} width={120} />
-          <Netlify className={tw(`m-12`)} width={140} />
-          <Nike className={tw(`m-12`)} width={140} />
-          <Figma className={tw(`m-12`)} width={140} />
+       
+          <aliexpress className={tw(`m-12`)} width={60} />
+          <amazon className={tw(`m-12`)} width={60} />
+          <uniqlo className={tw(`m-12`)} width={60} />
+          <wallmart className={tw(`m-12`)} width={60} />
+      
         </div>
       </div>
     </div>
