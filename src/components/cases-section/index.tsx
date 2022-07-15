d938ae83d537c584bf9ae7cdf6fe3098a37130c4
Rossi Kamal <rossikamal@gmail.com>
2022-07-16 03:37:21 +0600

    Update index.tsx

--- src/components/cases-section/index.tsx
+++ src/components/cases-section/index.tsx
@@ -45,20 +45,16 @@ const ParticleBg = () => (
 
 const articles = [
   {
-    title: `Velit reprehenderit culpa Lorem reprehenderit excepteur ipsum esse.`,
-    image: `/images/case-1.webp`,
-    alt: `Proident pariatur est.`,
+    title: `Bitcoin Wallet`,
+    image: `/images/b3.png`,
+    alt: `One though buy.`,
   },
   {
-    title: `Velit reprehenderit culpa Lorem reprehenderit ipsum esse.`,
-    image: `/images/case-2.webp`,
-    alt: `Proident pariatur est.`,
+    title: `Ethereum Wallet`,
+    image: `/images/b4.png`,
+    alt: `Fast transfer.`,
   },
-  {
-    title: `Velit reprehenderit culpa Lorem reprehenderit excepteur esse.`,
-    image: `/images/case-3.webp`,
-    alt: `Proident pariatur est.`,
-  },
+  
 ];
 
 const CasesSection = () => (
@@ -68,9 +64,9 @@ const CasesSection = () => (
         <ParticleBg />
       </div>
       <div className={tw(`max-w-7xl mx-4 lg:mx-auto pt-20 lg:pt-40`)}>
-        <h1 className={tw(`text-white text-4xl lg:text-7xl font-bold text-center`)}>What will you build?</h1>
+        <h1 className={tw(`text-white text-4xl lg:text-7xl font-bold text-center`)}>All products</h1>
         <p className={tw(`text-white text-gray-400 text-center text-xl mt-12`)}>
-          Don’t just take our word for it — see what leaders in digital are saying
+         We bring you latest cryptocurrency technology
         </p>
         <div className={tw(`mx-auto pt-24`)}>
           <div className={tw(`w-full flex flex-wrap justify-around`)}>
@@ -103,7 +99,7 @@ const CasesSection = () => (
                 text-indigo-400 cursor-pointer z-30 hover:text-indigo-600`,
               )}
             >
-              See all case studies
+             All Products
               <Arrow className={tw(`h-6 w-6 fill-current ml-2`)} />
             </span>
           </div>
