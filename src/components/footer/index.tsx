fe9449a085f3eaf02d01b27091d8da2ec818628d
kamalrossi <rossikamal@gmail.com>
2022-07-15 22:03:03 +0600

    first commit

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,48 @@
+import { tw } from 'twind';
+import Button from '@/components/button';
+
+const productLinks = [`Benefit`, `Plans`, `Testimonials`, `Pricing`, `Enterprise`, `What's new?`];
+const aboutLinks = [`Company History`, `Contact`];
+
+const Footer = () => (
+  <footer className={tw(`bg-white border-t border-gray-400 pt-14 pb-16`)}>
+    <div className={tw(`max-w-7xl mx-auto text-gray-400 px-8 lg:px-0 flex flex-wrap`)}>
+      <div className={tw(`mb-14 flex items-center w-full`)}>
+        <img className={tw(`h-12 w-12 mr-4`)} src="logo.svg" alt="logo" width={48} height={48} />
+        <p className={tw(`text-4xl text-indigo-500 font-bold`)}>GreenWallet</p>
+      </div>
+      <div className={tw(`w-full lg:w-1/2`)}>
+        <ul className={tw(`text-lg font-light flex flex-wrap w-full`)}>
+          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/3`)}>
+            <div>
+              <h4 className={tw(`text-gray-900 text-base font-bold mb-1`)}>Product</h4>
+              <ul>
+                {productLinks.map((link) => (
+                  <li className={tw(`text-gray-800 text-sm font-medium leading-6`)} key={link}>
+                    <a href="/">{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+          
+          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/3`)}>
+            <div>
+              <h4 className={tw(`text-gray-900 text-base font-bold mb-1`)}>About Us</h4>
+              <ul>
+                {aboutLinks.map((link) => (
+                  <li className={tw(`text-gray-800 text-sm font-medium leading-6`)} key={link}>
+                    <a href="/">{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  </footer>
+);
+
+export default Footer;
