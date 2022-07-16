6ce4f34dd9533c9205682345259080dc8d751508
Rossi Kamal <rossikamal@gmail.com>
2022-07-17 02:25:11 +0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,65 @@
-import { NextSeo } from 'next-seo';
-import Page from '@/components/page';
-import Header from '@/components/header';
-import VideoSection from '@/components/video-section';
-import ListSection from '@/components/list-section';
-import FeatureSection from '@/components/feature-section';
-import CasesSection from '@/components/cases-section';
-import SocialProof from '@/components/social-proof';
-import PricingTable from '@/components/pricing-table';
-import Footer from '@/components/footer';
+import { useEffect } from "react";
+import useUrl from "../component/hooks/useUrl";
+import Navbar3 from "../component/navbar3";
+import Offcanvas from "../component/offcanvas";
+import Section from "../component/scetion";
+import { useData } from "../store";
+import Product_Action from "../store/action/productAction";
 
-export default function Home() {
-  return (
-    <Page>
-      <NextSeo
-        title="GreenWallet"
-        description="A TypeScript/Next.js theme that includes everything you need to build amazing landing page!"
-      />
-      <Header />
-      <main>
-        <VideoSection />
-        <ListSection />
-        <FeatureSection />
-        <CasesSection />
-     
-        <PricingTable />
-      </main>
-      <Footer />
-    </Page>
-  );
+export async function getServerSideProps() {
+    let { url } = useUrl()
+    let res = await fetch(`${url}/product/all`)
+    let data = await res.json()
+    return {
+        props: {
+            products: data.allProduct
+        }
+    }
 }
+const Home = ({ products }) => {
+    let { dispatch, productState } = useData()
+
+    useEffect(() => {
+        dispatch({ type: Product_Action.SAVE_PRODUCT, payload: { allProducts: products } })
+        localStorage.setItem('__allProduct', JSON.stringify(products))
+        console.log(productState);
+    }, [])
+    let brandDetails = [
+        {
+            img: 'https://res.cloudinary.com/shimul/image/upload/v1614761599/logo-footer-main-1-1_l8ypig.png',
+            title: 'Prada'
+        },
+        {
+            img: 'https://res.cloudinary.com/shimul/image/upload/v1617824749/2xB1zxf81-download.jpg',
+            title: 'Levis'
+        },
+        {
+            img: 'https://res.cloudinary.com/shimul/image/upload/v1620947763/b8gisgu1o-download.jpg',
+            title: 'Gucci'
+        },
+        {
+            img: 'https://res.cloudinary.com/sium/image/upload/v1622573290/hero_dd9pzd.png',
+            title: 'ASOS'
+        },
+        {
+            img: 'https://res.cloudinary.com/sium/image/upload/v1622573467/realme_hatsni.jpg',
+            title: 'HNM'
+        },
+        
+    ]
+    return (
+        <>
+            <Navbar3 />
+            <Section product={brandDetails.slice(0, 5)} title='Famous brands' />
+            <Section product={products} link='all' title='Hot Products' />
+            <Section product={products.filter(sig => sig.category == 'men')} link='men' title='men' />
+            <Section product={products.filter(sig => sig.category == 'women')} link='women' title='women' />
+            <Section product={products.filter(sig => sig.category == 'kid')} link='kid' title='kid' />
+            
+            <p style={{ fontSize: '4rem', fontWeight: '700' }}>WellMall</p>
+        </>
+    )
+}
+
+
+export default Home;
